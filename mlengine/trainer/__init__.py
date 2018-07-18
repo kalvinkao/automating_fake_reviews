83ae66402c4b1b6fbd79625415e738d12bfccad1
muthderd <kaok@nala.roche.com>
2018-07-18 13:06:03 -0700

    updated init file

--- mlengine/trainer/__init__.py
+++ mlengine/trainer/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from trainer import model
-from trainer import util
+#from trainer import model
+#from trainer import util
